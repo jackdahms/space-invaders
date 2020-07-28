5aa5b3fc629e13fb7fb683c779b4c68eebc05675
Jack Dahms <jack.dahms@gmail.com>
2020-07-28 12:16:54 -0500

    initial commit

---
+++ Assets/GravityBody.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBody : MonoBehaviour {
+
+	public GravityAttractor attractor;
+	private Transform my_transform;
+
+    // Start is called before the first frame update
+    void Start() {
+    	GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+    	GetComponent<Rigidbody>().useGravity = false;
+        my_transform = transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+    	attractor.Attract(my_transform);   
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+
+    public float move_speed = 15;
+    private Vector3 move_dir;
+
+    public float horizontal_sensitivity = 10;
+    public float vertical_sensitivity = 5;
+    public bool invert_y_axis = true;
+
+    private Rigidbody my_body;
+    private Transform camera;
+
+    void Start() 
+    {
+        Cursor.visible = false;
+        my_body = GetComponent<Rigidbody>();
+        camera = transform.GetChild(0).transform;
+    }
+
+    void Update() 
+    {
+        move_dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+    }
+
+    void FixedUpdate() 
+    {
+        Vector3 local_dir = transform.TransformDirection(move_dir);
+        my_body.MovePosition(my_body.position + local_dir * move_speed * Time.deltaTime);
+
+        transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Mouse X") * horizontal_sensitivity, Vector3.up);
+
+        if (invert_y_axis)
+        {
+            camera.rotation *= Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * -vertical_sensitivity, Vector3.right);
+        }
+        else
+        {
+            camera.rotation *= Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * vertical_sensitivity, Vector3.right);
+        }
+    }
+}
